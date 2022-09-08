1ce64c9cfcf282914a8d08ed2c258ad45fe6df89
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-08 18:40:19 +0200

    Create a PlaySoundExtension with all the needed functions

--- PitchPerfect/PlaySoundViewController.swift
+++ PitchPerfect/PlaySoundViewController.swift
@@ -1,14 +1,16 @@
-//
-//  PlaySoundViewController.swift
-//  PitchPerfect
-//
-//  Created by Amjad Oudeh on 08.09.22.
-//
-
 import UIKit
+import AVFoundation
 
 class PlaySoundViewController: UIViewController {
     var recordedAudioURL: URL!
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
     // MARK: Outlets
     
     @IBOutlet weak var snailButton: UIButton!
@@ -25,18 +27,39 @@ class PlaySoundViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupAudio()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+    
     // MARK: Actions
-
+    
     @IBAction func playSoundForButton(_ sender: UIButton) {
         print("Play Sound Button Pressed")
-    }
+        switch(ButtonType(rawValue: sender.tag)!) {
+            case .slow:
+                playSound(rate: 0.5)
+            case .fast:
+                playSound(rate: 1.5)
+            case .chipmunk:
+                playSound(pitch: 1000)
+            case .vader:
+                playSound(pitch: -1000)
+            case .echo:
+                playSound(echo: true)
+            case .reverb:
+                playSound(reverb: true)
+            }
 
+            configureUI(.playing)
+    }
+    
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
         print("Stop Audio Button Pressed")
+        stopAudio()
     }
-
+    
 }
