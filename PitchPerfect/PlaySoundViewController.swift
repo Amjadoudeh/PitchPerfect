d3042cd4b8e8b86db0c49bb7ffce64d4152370fa
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-08 15:14:32 +0200

    Create PlaySoundViewController

---
+++ PitchPerfect/PlaySoundViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlaySoundViewController.swift
+//  PitchPerfect
+//
+//  Created by Amjad Oudeh on 08.09.22.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
