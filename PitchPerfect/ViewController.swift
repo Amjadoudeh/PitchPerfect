00b97b8b880bcff25a5a85237426797dfe706c80
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-08 14:03:24 +0200

    Create the first page with 2 buttons and 1 label

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -2,9 +2,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed")
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
 
